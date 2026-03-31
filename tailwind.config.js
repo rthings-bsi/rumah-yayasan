268e3a0a99dc7764ec28b37f1fd5a4000ae3c0f0
rthings <rickysatria442@gmail.com>
2026-04-01 01:43:28 +0700

    Update Fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js', // <-- Tambahkan baris ini
+        './resources/js/**/*.vue', // <-- (Opsional) Tambahkan jika kamu pakai Vue
+        './resources/js/**/*.jsx', // <-- (Opsional) Tambahkan jika kamu pakai React
     ],
 
     theme: {
